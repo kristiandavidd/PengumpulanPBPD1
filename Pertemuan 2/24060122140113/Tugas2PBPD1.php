c8f92517704dc0fb33269584ccd39723b2d66674
Bima1134 <160698225+Bima1134@users.noreply.github.com>
2024-09-10 22:02:34 +0700

    Update Tugas2PBPD1.php
    Menghapus elemen array bernama Farrel ardana

--- Pertemuan 2/24060122140113/Tugas2PBPD1.php
+++ Pertemuan 2/24060122140113/Tugas2PBPD1.php
@@ -15,8 +15,7 @@
                 'Budi' => array(78, 60, 64),
                 'Nina' => array(67, 56, 84),
                 'Budi' => array(87, 69, 50),
-                'Budi' => array(98, 65, 74),
-                'Farrel Ardana' => array(100,100,20)
+                'Budi' => array(98, 65, 74)
             );
 
             // Fungsi untuk menghitung rata-rata
