d12999059b54fc70a43cf1cf2d2e14c7ea724e90
Bima1134 <160698225+Bima1134@users.noreply.github.com>
2024-09-18 00:10:50 +0700

    Update Tugas3PBPD1_24060122140113.php

--- Pertemuan 3/24060122140113/Tugas3PBPD1_24060122140113.php
+++ Pertemuan 3/24060122140113/Tugas3PBPD1_24060122140113.php
@@ -5,6 +5,8 @@ Deskripsi : Form pengisian Siswa SMA dengan input NIS, Nama, Jenis Kelamin, Kela
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Siswa</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
