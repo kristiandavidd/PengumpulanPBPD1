a8a8fd7d6264849235a076a9108539dec41edc85
Nashwan Adenaya <75899276+OneRicez@users.noreply.github.com>
2024-09-24 20:47:08 +0700

    Add Tugas 4 - Nashwan

---
+++ Pertemuan 4/24060122130084/view_books.php
@@ -0,0 +1,50 @@
+<?php include('./header.html') ?>
+<!-- 
+Nama/NIM Pembuat  : Nashwan Adenaya / 24060122130084
+Tanggal Pembuatan : 20 September 2024
+File              : view_books.php
+Deskripsi         : tampilan untuk list buku yang tersedia
+ -->
+<p class="mt-3"><a href="admin.php" >Back to admin</a></p>
+<div class="card mt-2">
+    <div class="card-header">Books Data</div>
+    <div class="card-body">
+        <table class="table table-striped">
+            <tr>
+                <th>ISBN</th>
+                <th>Author</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Action</th>
+            </tr>
+            <?php
+            require_once('./lib/db_login.php');
+
+            // TODO 1: Tuliskan dan eksekusi query
+            $query = "SELECT * FROM books ORDER BY isbn";
+            $result = $db->query($query);
+            if (!$result) {
+                die("Could not query the database: <br/>" . $db->error . "<br>Query: " . $query);
+            }
+            //execute the query
+            while ($row = $result->fetch_object()) {
+                echo '<tr>';
+                echo '<td>' . $row->isbn . '</td>';
+                echo '<td>' . $row->author . '</td>';
+                echo '<td>' . $row->title . '</td>';
+                echo '<td>' . $row->price . '</td>';
+                echo '<td><a class="btn btn-primary" href="show_cart.php?id=' . $row->isbn . '">Add to Cart<a/></td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+            echo '<br/>';
+            echo 'Total Rows= ' . $result->num_rows;
+            $result->free();
+            $db->close();
+            ?>
+
+            <br/><br/>
+            <a class="btn btn-primary" href="show_cart.php">Show Cart</a>
+    </div>
+</div>
+<?php include('./footer.html') ?>
