a7ae0e27c8c30dd9d9c7f7b343cd534691f58038
ardyhra <145884823+ardyhra@users.noreply.github.com>
2024-09-25 08:40:06 +0700

    Revisi add_customer.php dan edit_customer.php

--- Pertemuan 4/24060122130053/view/edit_customer.php
+++ Pertemuan 4/24060122130053/view/edit_customer.php
@@ -14,55 +14,57 @@ $id = $_GET['id']; //mendapatkan customerid yang dilewatkan ke url
 
 //mengecek apakah user belum menekan tombol submit
 if (!isset($_POST["submit"])) {
-        $query = "SELECT * FROM customers WHERE customerid=".$id." "; // Execute the query
-        $result = $db->query($query);
-        if (!$result) {
-        die ("Could not query the database: <br />". $db->error); }
-        else{
-            while ($row = $result->fetch_object()) {
+    $query = "SELECT * FROM customers WHERE customerid=".$id." "; // Execute the query
+    $result = $db->query($query);
+    if (!$result) {
+        die ("Could not query the database: <br />". $db->error); 
+    } else {
+        while ($row = $result->fetch_object()) {
             $name = $row->name;
             $address = $row->address;
             $city = $row->city;
-            }
         }
-}
-else{
+    }
+} else {
     $valid = TRUE; //flag validasi
-    $name = test_input ($_POST['name']); 
-    if ($name == ''){
+    $name = test_input($_POST['name']); 
+    if ($name == '') {
         $error_name = "Name is required";
         $valid = FALSE;
-    }elseif (!preg_match("/^[a-zA-Z\s'-]*$/", $name)) { 
+    } elseif (!preg_match("/^[a-zA-Z\s'-]*$/", $name)) { 
         $error_name = "Only letters and white space allowed"; 
         $valid = FALSE;
     }
+
     $address = test_input($_POST['address']);
     if ($address == '') {
-    $error_address = "Address is required";
-    $valid = FALSE;
-    
-    $city = $_POST['city'];
-    if ($city =='' || $city=='none') {
-    $error_city = "city is required";
-    $valid = FALSE;
+        $error_address = "Address is required";
+        $valid = FALSE;
     }
-    //update data into database
+
+    $city = $_POST['city'];
+    if ($city == '' || $city == 'none') {
+        $error_city = "City is required";
+        $valid = FALSE;
+    }
+
+    // If the form is valid, update the data into the database
     if ($valid) {
-        //Asign a query
-        //escape inputs data
-        $address=$db->real_escape_string($address);
-        //Asign a query
+        //Escape inputs data
+        $address = $db->real_escape_string($address);
+
+        // Asign a query
         $query = "UPDATE customers SET name= '".$name."', address='".$address."', city='".$city."' WHERE customerid=".$id." ";
+        
         // Execute the query
         $result = $db->query($query);
         if (!$result) {
-            die ("Could not query the database: <br />". $db->error. '<br>Query: ' . $query
-            );
-        }else{
-        $db->close();
-        header('Location: view_customer.php');
+            die("Could not query the database: <br />". $db->error. '<br>Query: ' . $query);
+        } else {
+            $db->close();
+            header('Location: view_customer.php'); // Redirect to view_customer.php
+            exit(); // Make sure the script stops here after redirect
         }
-    }
     }
 }
 ?>
@@ -72,30 +74,37 @@ else{
 <div class="card">
 <div class="card-header">Edit Customers Data</div>
 <div class="card-body">
-<form method="POST" autocomplete="on" action="<?php echo htmlspecialchars($_SERVER[ "PHP_SELF"]).'?id='.$id; ?>">
-<div class="form-group">
-<label for="name">Nama: </label>
-<input type="text" class="form-control" id="name" name="name" value="<?php echo $name; ?>">
-<div class="error"><?php if (isset($error_name)) echo $error_name; ?></div> </div>
-<div class="form-group">
-<label for="address">Address:</label>
-<textarea class="form-control" id="address" name="address" rows="5"><?php echo $address; ?></textarea>
-<div class="error"><?php if (isset($error_address)) echo $error_address; ?></div> </div>
-<div class="form-group">
-<label for="city">City:</label>
-<select name="city" id="city" class="form-control" required>
-<option value="none" <?php if (!isset($city)) echo 'selected="true"'; ?>> --Select a city--</option>
-<option value="Airport West" <?php if (isset($city) && $city=="Airport West") echo 'selected="true"'; ?>>Airport West</option>
-<option value="Box Hill" <?php if (isset($city) && $city=="Box Hill") echo 'selected="true"'; ?>>Box Hill</option>
-<option value="Yarraville" <?php if (isset($city) && $city=="Yarraville") echo 'selected="true"'; ?>>Yarraville</option>
-</select>
-<div class="error"><?php if (isset($error_city)) echo $error_city; ?></div> </div> <br>
-<button type="submit" class="btn btn-primary" name="submit" value="submit">Submit </button>
-<a href="view_customer.php" class="btn btn-secondary">Cancel</a>
-</table>
+<form method="POST" autocomplete="on" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]).'?id='.$id; ?>">
+    <div class="form-group">
+        <label for="name">Nama: </label>
+        <input type="text" class="form-control" id="name" name="name" value="<?php echo $name; ?>">
+        <div class="error"><?php if (isset($error_name)) echo $error_name; ?></div>
+    </div>
+
+    <div class="form-group">
+        <label for="address">Address:</label>
+        <textarea class="form-control" id="address" name="address" rows="5"><?php echo $address; ?></textarea>
+        <div class="error"><?php if (isset($error_address)) echo $error_address; ?></div>
+    </div>
+
+    <div class="form-group">
+        <label for="city">City:</label>
+        <select name="city" id="city" class="form-control" required>
+            <option value="none" <?php if (!isset($city)) echo 'selected="true"'; ?>> --Select a city--</option>
+            <option value="Airport West" <?php if (isset($city) && $city=="Airport West") echo 'selected="true"'; ?>>Airport West</option>
+            <option value="Box Hill" <?php if (isset($city) && $city=="Box Hill") echo 'selected="true"'; ?>>Box Hill</option>
+            <option value="Yarraville" <?php if (isset($city) && $city=="Yarraville") echo 'selected="true"'; ?>>Yarraville</option>
+        </select>
+        <div class="error"><?php if (isset($error_city)) echo $error_city; ?></div>
+    </div> 
+    <br>
+    <button type="submit" class="btn btn-primary" name="submit" value="submit">Submit</button>
+    <a href="view_customer.php" class="btn btn-secondary">Cancel</a>
 </form>
 </div>
 </div>
-<?php include('../footer.html') ?>
+<?php include('../footer.html'); ?>
+
 <?php
 $db->close();
+?>
