1858cedcb955ae1093f9b6ffb08175a9a4c9a850
Zikry Alfahri <alfarakram@gmail.com>
2024-09-29 02:51:24 +0700

    Tugas 5 - 24060122120033

---
+++ Pertemuan 5/24060122120033/view/get_server_time.php
@@ -0,0 +1,12 @@
+<!--
+    Nama                : Zikry Alfahri Akram
+    NIM                 : 24060122120033
+    Tanggal Pengerjaan  : Kamis, 26 September 2024
+    Nama File           : get_server_time.php
+-->
+
+<?php 
+    // Menampilkan waktu server
+    echo date('H:i:s');
+?>
+
