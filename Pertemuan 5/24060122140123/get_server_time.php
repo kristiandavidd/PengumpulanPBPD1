34f8bfccf34b3cca820abe53ef2ce4671ff0f834
capinowo <alyasafina507@gmail.com>
2024-10-01 23:26:35 +0700

    Pert 5

---
+++ Pertemuan 5/24060122140123/get_server_time.php
@@ -0,0 +1,11 @@
+<!-- 
+Nama                   : Alya Safina
+NIM                    : 24060122140123
+-->
+
+<?php 
+    // TODO 1: Tuliskan response berupa waktu server saat ini
+    echo date('H:i:s');
+
+?>
+
