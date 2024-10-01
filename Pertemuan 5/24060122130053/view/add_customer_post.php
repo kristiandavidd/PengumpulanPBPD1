bc4053b4e3dd3ef9d8e1609f08ba2eecf32dfebc
ardyhra <145884823+ardyhra@users.noreply.github.com>
2024-10-02 06:49:12 +0700

    Pengumpulan Tugas 5 - 24060122130053

---
+++ Pertemuan 5/24060122130053/view/add_customer_post.php
@@ -0,0 +1,30 @@
+<!-- 
+Nama : Ardy Hasan Rona Akhmad
+NIM : 24060122130053
+Tanggal : 2 Oktober 2024
+Lab : PBP D1
+Tugas Pertemuan 5
+-->
+
+<?php
+require_once('../lib/db_login.php');
+
+$name = $db->real_escape_string($_POST['name']);
+$address = $db->real_escape_string($_POST['address']);
+$city = $db->real_escape_string($_POST['city']);
+
+// Assign a query
+$query = "INSERT INTO customers (name, address, city) VALUES ('" . $name . "', '" . $address . "', '" . $city . "')";
+$result = $db->query($query);
+
+if (!$result) {
+    echo 'div class="alert alert-danger alert-dismissible"><strong>Error! </strong> Could not query the database <br>'.$db->error.'<br></div>';
+} else {
+    echo '<div class="alert alert-success alert-dismissible"><strong>Success! </strong> Data has been added.<br>
+    Name: '.$_POST['name'].'<br>
+    Address: '.$_POST['address'].'<br>
+    City: '.$_POST['city'].'<br></div>';
+}
+
+// Close DB Connection
+$db->close();
