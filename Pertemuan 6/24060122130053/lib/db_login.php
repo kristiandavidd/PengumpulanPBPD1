cab675f008b681ff81bc60e7761a959262ccf624
ardyhra <145884823+ardyhra@users.noreply.github.com>
2024-10-02 09:14:31 +0700

    Add files via upload

---
+++ Pertemuan 6/24060122130053/lib/db_login.php
@@ -0,0 +1,23 @@
+<!-- 
+Nama : Ardy Hasan Rona Akhmad
+NIM : 24060122130053
+Tanggal : 2 Oktober 2024
+Lab : PBP D1
+Responsi
+-->
+
+<?php
+$db_host='localhost'; 
+$db_database='profilebook'; 
+$db_username='root'; 
+$db_password='';
+// connect database
+$db = new mysqli($db_host, $db_username, $db_password, $db_database, 3308); 
+if ($db->connect_errno) {
+    die ("Could not connect to the database: <br />". $db->connect_error);
+}
+function test_input ($data) {
+$data = trim($data);
+$data = stripslashes ($data); $data = htmlspecialchars ($data); 
+return $data;
+}
